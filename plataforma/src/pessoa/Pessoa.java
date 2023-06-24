2d70562c9fd9106fba49f8c0026070378e1d7f0e
USUÁRIO <USURIO@host.docker.internal>
2023-06-24 00:51:09 -0300

    incluir dois atributos, cpf e situação

--- plataforma/src/pessoa/Pessoa.java
+++ plataforma/src/pessoa/Pessoa.java
@@ -5,8 +5,22 @@ public class Pessoa {
 	private String sobrenome;
 	public int idade;
 	public boolean situacao = true;
+	public String cpf;
 	
 		
+	
+	public boolean isSituacao() {
+		return situacao;
+	}
+	public void setSituacao(boolean situacao) {
+		this.situacao = situacao;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 	public int getIdade() {
 		return idade;
 	}
